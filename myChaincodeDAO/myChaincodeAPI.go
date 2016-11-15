ea17f58d4cd525d9220073b437d10704b44661c6
aaronali <aaronali780@gmail.com>
2016-11-15 11:22:21 -0500

    update

--- myChaincodeDAO/myChaincodeAPI.go
+++ myChaincodeDAO/myChaincodeAPI.go
@@ -15,12 +15,9 @@ specific language governing permissions and limitations
 under the License.
 */
 
-
 //todo:  need to make consitent status.  need better way to take them out of the process when closed
 //todo: add application security to get user names
 //todo:  make user into account
-
-
 
 package main
 
@@ -28,38 +25,33 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
-
-
-
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
-	userRep			UserRepository 
-	holdingsRep		HoldingsRepository
-	securitiesRep	SecurityRepository
-	tradeRep		TradeRepository
-	stub			*shim.ChaincodeStub
+	userRep       UserRepository
+	holdingsRep   HoldingsRepository
+	securitiesRep SecurityRepository
+	tradeRep      TradeRepository
+	stub          *shim.ChaincodeStub
 }
-
 
 //********************************************************************************************************
 //****      Blockchain API functions                                                                  ****
 //********************************************************************************************************
 
-
 func (t *SimpleChaincode) initObjects(stub *shim.ChaincodeStub) error {
 	t.stub = stub
 	t.writeOut("in init objects")
-	
-	
+
 	//initialize our repositories
 	t.userRep.init(stub)
 	t.holdingsRep.init(stub)
 	t.securitiesRep.init(stub)
 	t.tradeRep.init(stub)
-	
+
 	return nil
 }
 
@@ -67,35 +59,32 @@ func (t *SimpleChaincode) initObjects(stub *shim.ChaincodeStub) error {
 func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Init called, initializing chaincode")
 
-
 	//initialize our repositories
 	t.initObjects(stub)
-	
+
 	t.writeOut("in init")
-		
-	
-	//Register some users.  this would normally happen via the UI but we will do it here to simplify	
+
+	//Register some users.  this would normally happen via the UI but we will do it here to simplify
 	_, err := t.registerUser("BANK")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerUser("David")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerUser("Wesley")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerUser("Aaron")
 	if err != nil {
 		return nil, err
 	}
-	
-	
+
 	//register our securities and offer them for sale
 	_, err = t.registerSecurity("JaimeKilled", "Jaime gets killed")
 	if err != nil {
@@ -105,7 +94,7 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerSecurity("JonKilled", "Jon gets killed")
 	if err != nil {
 		return nil, err
@@ -114,36 +103,33 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 	if err != nil {
 		return nil, err
 	}
-	
-	
-	
+
 	//the bank does an IPO
 	_, err = t.registerTrade("ask", "BANK", "JaimeKilled", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerTrade("ask", "BANK", "JaimeKiller", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerTrade("ask", "BANK", "JonKilled", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	_, err = t.registerTrade("ask", "BANK", "JonKiller", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
-	
+
 	_, err = t.registerTrade("bid", "Aaron", "JaimeKilled", defaultPrice, 100, "")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	t.writeOut("Before dividend")
 	//offer payoff anyone with Jaime,Killed (Aaron)
 	_, err = t.dividend("JaimeKilled", 50)
@@ -151,18 +137,15 @@ func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args [
 		t.writeOut("in init: after dividend in err != nil")
 		return nil, err
 	}
-	
-	
+
 	t.writeOut("Before return")
 	return nil, nil
 }
 
-
-
 // Invoke callback representing the invocation of a chaincode
 func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Invoke called, determining function")
-	
+
 	t.initObjects(stub) //for some reason the stub changes each call
 	// Handle different functions
 	if function == "init" {
@@ -171,26 +154,26 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	} else if function == "ask" || function == "bid" {
 		// offers squares up for sale as initial public offering
 		fmt.Printf("Function is trade")
-		
+
 		if len(args) != 5 {
 			return nil, errors.New("Incorrect number of arguments. Expecting registerTrade(tradeType, userid, security, price, units, expiry)")
 		}
-		
+
 		price, err := strconv.ParseFloat(args[2], 64)
 		if err != nil {
 			return nil, err
 		}
-		
+
 		units, err := strconv.Atoi(args[3])
 		if err != nil {
 			return nil, err
 		}
-		
+
 		return t.registerTrade(function, args[0], args[1], price, units, args[4])
 	} else if function == "dividend" {
 		// enter an an character event happening in the show.  pays out to users holding squares
 		fmt.Printf("Function is ask")
-		
+
 		amount, err := strconv.Atoi(args[1])
 		if err != nil {
 			return nil, err
@@ -206,25 +189,22 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 		userID := args[0]
 		return t.registerUser(userID)
 	}
-	
+
 	return nil, errors.New("Received unknown function invocation")
 }
-
-
-
 
 // Query callback representing the query of a chaincode
 func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	fmt.Printf("Query called, determining function")
-	
+
 	t.initObjects(stub)
 	// Handle different functions
 	if function == "holdings" {
 		// query a users holdings
-		return t.holdings(args[0])  //userID
+		return t.holdings(args[0]) //userID
 	} else if function == "ballance" {
 		// query a users cash on hand
-		return t.ballance(stub, args[0])	//userID
+		return t.ballance(stub, args[0]) //userID
 	} else if function == "users" {
 		// query for list of users
 		return t.users()
@@ -234,18 +214,14 @@ func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args 
 	} else {
 		fmt.Printf("Function is query")
 		return nil, errors.New("Invalid query function name. Expecting holdings, ballance, users or securities")
-	}	
-	
+	}
+
 	return nil, nil
 }
-
-
-
-
 
 func main() {
 	err := shim.Start(new(SimpleChaincode))
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
-}  
+}
