71ac5a83a8b1ac0e03addb6d0d483e99ad84f044
aaronali <aaronali780@gmail.com>
2016-11-21 12:33:56 -0500

    commit

--- DavidsCode/queryBallance.bat
+++ DavidsCode/queryBallance.bat
@@ -8,6 +8,6 @@ for /f "delims=" %%x in (serviceKey.txt) do set Keytext=%%x
 curl -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d "{\"jsonrpc\": \"2.0\",\"method\": \"query\",\"params\": {\"type\": 1,\"chaincodeID\": {\"name\": \"%Keytext:~52,128%\"},\"ctorMsg\": {\"function\": \"ballance\",\"args\": [\"Aaron\"]},\"secureContext\": \"user_type1_0\"},\"id\": 1}" "https://d48e4ff54a324330ac90c7ed2a4ddaa2-vp1.us.blockchain.ibm.com:444/chaincode"
 @echo. 
 
-pause
+ 
 
 goto :QUERY
