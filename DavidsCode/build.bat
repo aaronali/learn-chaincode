cbaecf05775e2e769195d1f2014039e17c1d0296
aaronali <aaronali780@gmail.com>
2016-11-22 08:38:53 -0500

    commit

--- DavidsCode/build.bat
+++ DavidsCode/build.bat
@@ -10,8 +10,8 @@ rem IF NOT ERRORLEVEL 1 (
 	go clean ./
 	git commit -m "commit" -a
 	git push
-	curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://d48e4ff54a324330ac90c7ed2a4ddaa2-vp1.us.blockchain.ibm.com:444/chaincode" > serviceKey.txt
-rem )
+	curl.exe -X POST --insecure --header "Content-Type: application/json" --header "Accept: application/json" -d @deploy.json "https://22971498235e45ff9ca17cb7163e8205-vp0.us.blockchain.ibm.com:5001/chaincode" > serviceKey.txt
+
 
 
 pause
